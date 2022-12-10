182b71adadc99b55b9c29f27e07b3af71ec34c38
Kladnitsky Danil <danyakladnitsky@gmail.com>
2022-12-10 16:27:14 +0300

    add frontend

--- api-gateway/src/routes/users/users.controller.ts
+++ api-gateway/src/routes/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Inject, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Inject,
+  Query,
+} from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { JwtPayload } from 'jsonwebtoken';
 import { MICROSERVICES } from 'src/common/const/microservices';
@@ -9,8 +15,19 @@ export class UsersController {
     @Inject(MICROSERVICES.AUTH_MICROSERVICE) private authClient: ClientProxy,
   ) {}
 
-  @Get('verify')
-  async verifyUser(@Query('token') payload: JwtPayload) {
-    return this.authClient.send({ cmd: 'verify.token' }, payload);
+  @Get('token/verify')
+  async verifyUser(@Query('token') token: JwtPayload) {
+    return this.authClient.send({ cmd: 'verify.token' }, { token });
+  }
+
+  @Get('token')
+  async getToken(@Query() payload: JwtPayload) {
+    try {
+      return await this.authClient
+        .send({ cmd: 'decode.token' }, payload)
+        .subscribe();
+    } catch (error) {
+      throw new BadRequestException('sss');
+    }
   }
 }
--- auth-service/src/routes/token/token.controller.ts
+++ auth-service/src/routes/token/token.controller.ts
@@ -1,21 +1,17 @@
 import { Controller, Query } from '@nestjs/common';
 import { decodeJWT } from 'src/providers/jwt.provider';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { JwtPayload } from 'jsonwebtoken';
 
 @Controller('token')
 export class TokenController {
   @MessagePattern({ cmd: 'verify.token' })
-  async verifyToken(@Query('token') token: string): Promise<boolean> {
-    console.log(token);
-
+  async verifyToken(@Payload('token') token: string): Promise<boolean> {
     return Boolean(decodeJWT(token));
   }
 
   @MessagePattern({ cmd: 'decode.token' })
-  async decodeToken(
-    @Query('token') token: string,
-  ): Promise<JwtPayload | string> {
+  async decodeToken(@Payload() token: string): Promise<JwtPayload | string> {
     return decodeJWT(token);
   }
 }
