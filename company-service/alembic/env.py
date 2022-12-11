97eb019f84bc83db8fe61b82de6eff72c80cb984
Kladnitsky Danil <danyakladnitsky@gmail.com>
2022-12-11 12:52:06 +0300

    fix env

--- company-service/alembic/env.py
+++ company-service/alembic/env.py
@@ -15,7 +15,7 @@ sys.path.append(BASE_DIR)
 
 config = context.config
 
-config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
+config.set_main_option("sqlalchemy.url", os.environ["COMPANY_DATABASE_URL"])
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
